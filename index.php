50f55c4d2dbf080e82d6be3fbb29bfa5c71e6b4f
jonathanwang2018 <71296035+jonathanwang2018@users.noreply.github.com>
2020-11-25 06:43:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ You may not redistribute this code without the following conditions.
 <html lang="en-US" dir="ltr">
   <head>
     <title>URL</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="resources/style.css" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,200&display=swap" rel="stylesheet">
   </head>
   <body>
@@ -29,31 +29,6 @@ You may not redistribute this code without the following conditions.
 </form>
 </div>
 
-<?php
-$path = 's/';
-
-/* foreach (glob($path.'.txt') as $file) { */
-foreach (glob($path.'*') as $file) {
-    
-    if(time() - filectime($file) > 2592000){
-      unlink($file);
-    }
-}
-?>
-
-
-<!--<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-<script>
- window.OneSignal = window.OneSignal || [];
- OneSignal.push(function() {
-   OneSignal.init({
-     appId: "d4040d1f-09b6-4efe-938c-1daaa74afa02",
-     notifyButton: {
-       enable: true,
-     },
-   });
- });
-</script>-->
 
   </body>
 </html>
